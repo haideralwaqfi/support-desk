bde05cf0020bd1b7fb00e0f60c8a1adade562c8b
haideralwaqfi <haideralwaqfi@gmail.com>
2023-02-10 14:53:37 +0300

    project template

---
+++ backend/controllers/userController.js
@@ -0,0 +1,78 @@
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !password || !email) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  //find if already exist
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("user already exist");
+  }
+
+  //Hash
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new error("Inavaild user data");
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Creadintials");
+  }
+});
+
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
+//genertae Token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log("server...");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+//conntect to db
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello from the backend" });
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
